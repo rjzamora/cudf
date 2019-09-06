327de045988b5b962c2ebc4cbe4072cb53c99060
rjzamora <rzamora217@gmail.com>
2019-09-06 07:43:57 -0700

    reverting to values_host

--- python/dask_cudf/dask_cudf/backends.py
+++ python/dask_cudf/dask_cudf/backends.py
@@ -1,10 +1,10 @@
-from functools import wraps
+# from functools import wraps
 
-import cupy
+# import cupy
 import numpy as np
 from pandas._libs.algos import groupsort_indexer
 
-from dask.compatibility import Iterator
+# from dask.compatibility import Iterator
 from dask.dataframe.core import get_parallel_type, make_meta, meta_nonempty
 from dask.dataframe.methods import (
     concat_dispatch,
@@ -79,76 +79,25 @@ def group_split_2_cudf(df, col):
 
 @percentiles_summary.register(cudf.Series)
 def percentiles_summary_cudf(df, num_old, num_new, upsample, state):
-    def _sample_percentiles(
-        num_old, num_new, chunk_length, upsample=1.0, random_state=None
-    ):
-        # *waves hands*
-        random_percentage = 1 / (1 + (4 * num_new / num_old) ** 0.5)
-        num_percentiles = upsample * num_new * (num_old + 22) ** 0.55 / num_old
-        num_fixed = int(num_percentiles * (1 - random_percentage)) + 2
-        num_random = int(num_percentiles * random_percentage) + 2
-
-        if num_fixed + num_random + 5 >= chunk_length:
-            return cupy.linspace(0, 100, chunk_length + 1)
-
-        if not isinstance(random_state, cupy.random.RandomState):
-            random_state = cupy.random.RandomState(random_state)
-
-        q_fixed = cupy.linspace(0, 100, num_fixed)
-        q_random = random_state.rand(num_random) * 100
-        q_edges = cupy.asarray(
-            [60 / (num_fixed - 1), 100 - 60 / (num_fixed - 1)]
-        )
-        qs = cupy.concatenate(
-            [q_fixed, q_random, q_edges, cupy.asarray([0, 100])]
-        )
-        qs.sort()
-        # Make the divisions between percentiles a little more even
-        qs = 0.5 * (qs[:-1] + qs[1:])
-        return qs
-
-    @wraps(cupy.percentile)
-    def _percentile(a, q, interpolation="linear"):
-        n = len(a)
-        if not len(a):
-            return None, n
-        if isinstance(q, Iterator):
-            q = list(q)
-        if a.dtype.name == "category":
-            result = cupy.percentile(a.codes, q, interpolation=interpolation)
-            import pandas as pd
-
-            return (
-                pd.Categorical.from_codes(result, a.categories, a.ordered),
-                n,
-            )
-        if cupy.issubdtype(a.dtype, np.datetime64):
-            a2 = a.astype("i8")
-            result = cupy.percentile(a2, q, interpolation=interpolation)
-            return result.astype(a.dtype), n
-        if not cupy.issubdtype(a.dtype, cupy.number):
-            interpolation = "nearest"
-        return cupy.percentile(a, q, interpolation=interpolation), n
-
-    def _percentiles_to_weights(qs, vals, length):
-        if length == 0:
-            return ()
-        diff = np.ediff1d(qs, 0.0, 0.0)
-        weights = 0.5 * length * (diff[1:] + diff[:-1])
-        return vals.tolist(), weights.tolist()
+    from dask.dataframe.partitionquantiles import (
+        sample_percentiles,
+        percentiles_to_weights,
+    )
+    from dask.array.percentile import _percentile
 
     length = len(df)
     if length == 0:
         return ()
-    random_state = cupy.random.RandomState(state)
-    qs = _sample_percentiles(num_old, num_new, length, upsample, random_state)
-    data = df.values
+    random_state = np.random.RandomState(state)
+    qs = sample_percentiles(num_old, num_new, length, upsample, random_state)
+    data = df.values_host
     interpolation = "linear"
     if is_categorical_dtype(data):
         data = data.codes
         interpolation = "nearest"
-    vals, _ = _percentile(data, qs, interpolation=interpolation)
-    if interpolation == "linear" and cupy.issubdtype(data.dtype, cupy.integer):
-        vals = cupy.around(vals).astype(data.dtype)
-    vals_and_weights = _percentiles_to_weights(qs.get(), vals, length)
+    vals, _ = _percentile(data.get(), qs, interpolation=interpolation)
+    if interpolation == "linear" and np.issubdtype(data.dtype, np.integer):
+        vals = np.around(vals).astype(data.dtype)
+    vals_and_weights = percentiles_to_weights(qs, vals, length)
+    print(vals_and_weights)
     return vals_and_weights
