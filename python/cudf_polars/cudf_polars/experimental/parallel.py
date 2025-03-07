044b4be1d9165cddd7ab47b561601f352fe01de7
rjzamora <rzamora217@gmail.com>
2025-03-07 13:16:44 -0800

    save work

--- python/cudf_polars/cudf_polars/experimental/parallel.py
+++ python/cudf_polars/cudf_polars/experimental/parallel.py
@@ -13,7 +13,16 @@ import cudf_polars.experimental.io
 import cudf_polars.experimental.join
 import cudf_polars.experimental.select
 import cudf_polars.experimental.shuffle  # noqa: F401
-from cudf_polars.dsl.ir import IR, Cache, Filter, HStack, Projection, Select, Union
+from cudf_polars.dsl.ir import (
+    IR,
+    Cache,
+    Filter,
+    HStack,
+    Projection,
+    Select,
+    Sort,
+    Union,
+)
 from cudf_polars.dsl.traversal import CachingVisitor, traversal
 from cudf_polars.experimental.base import PartitionInfo, _concat, get_key_name
 from cudf_polars.experimental.dispatch import (
@@ -301,3 +310,4 @@ generate_ir_tasks.register(Cache, _generate_ir_tasks_pwise)
 generate_ir_tasks.register(Filter, _generate_ir_tasks_pwise)
 generate_ir_tasks.register(HStack, _generate_ir_tasks_pwise)
 generate_ir_tasks.register(Select, _generate_ir_tasks_pwise)
+generate_ir_tasks.register(Sort, _generate_ir_tasks_pwise)
--- python/cudf_polars/cudf_polars/experimental/shuffle.py
+++ python/cudf_polars/cudf_polars/experimental/shuffle.py
@@ -8,11 +8,12 @@ import json
 import operator
 from typing import TYPE_CHECKING, Any
 
+import numpy as np
 import pyarrow as pa
 
 import pylibcudf as plc
 
-from cudf_polars.containers import DataFrame
+from cudf_polars.containers import Column, DataFrame
 from cudf_polars.dsl.ir import IR, Sort
 from cudf_polars.experimental.base import _concat, get_key_name
 from cudf_polars.experimental.dispatch import generate_ir_tasks, lower_ir_node
@@ -78,13 +79,62 @@ class Shuffle(IR):
         return df
 
 
+class OrderedShuffle(IR):
+    """Shuffle multi-partition data by column order."""
+
+    __slots__ = ("by", "null_order", "options", "order")
+    _non_child = ("schema", "by", "order", "null_order", "options")
+    by: NamedExpr
+    """Sort key."""
+    order: plc.types.Order
+    """Sort order."""
+    null_order: plc.types.NullOrder
+    """Null sorting location."""
+    options: dict[str, Any]
+    """Shuffling options."""
+
+    def __init__(
+        self,
+        schema: Schema,
+        by: NamedExpr,
+        order: plc.types.Order,
+        null_order: plc.types.NullOrder,
+        options: dict[str, Any],
+        df: IR,
+    ):
+        self.schema = schema
+        self.by = by
+        self.order = order
+        self.null_order = null_order
+        self.options = options
+        self._non_child_args = (
+            by,
+            order,
+            null_order,
+            options,
+        )
+        self.children = (df,)
+
+    def get_hashable(self) -> Hashable:
+        """Hashable representation of the node."""
+        return (
+            type(self),
+            tuple(self.schema.items()),
+            self.by,
+            self.order,
+            self.null_order,
+            json.dumps(self.options),
+            self.children,
+        )
+
+
 def _partition_dataframe(
     df: DataFrame,
     keys: tuple[NamedExpr, ...],
     count: int,
 ) -> dict[int, DataFrame]:
     """
-    Partition an input DataFrame for shuffling.
+    Partition an input DataFrame for hash shuffling.
 
     Notes
     -----
@@ -132,9 +182,9 @@ def _partition_dataframe(
     }
 
 
-def _simple_shuffle_graph(
-    name_out: str,
+def _hash_shuffle_graph(
     name_in: str,
+    name_out: str,
     keys: tuple[NamedExpr, ...],
     count_in: int,
     count_out: int,
@@ -195,9 +245,9 @@ def _(
     # Use a simple all-to-all shuffle graph.
 
     # TODO: Optionally use rapidsmp.
-    return _simple_shuffle_graph(
-        get_key_name(ir),
+    return _hash_shuffle_graph(
         get_key_name(ir.children[0]),
+        get_key_name(ir),
         ir.keys,
         partition_info[ir.children[0]].count,
         partition_info[ir].count,
@@ -206,30 +256,120 @@ def _(
 
 @lower_ir_node.register(Sort)
 def _(
-    ir: Shuffle, rec: LowerIRTransformer
+    ir: Sort, rec: LowerIRTransformer
 ) -> tuple[IR, MutableMapping[IR, PartitionInfo]]:
     from cudf_polars.experimental.parallel import PartitionInfo
 
     (child,) = ir.children
     new_child, pi = rec(child)
+
+    # Reconstruct new Sort node
     new_node = ir.reconstruct([new_child])
-    pi[new_node] = PartitionInfo(
-        # Default sort preserves partition count
-        count=pi[new_child].count
-    )
+    count_out = pi[new_child].count
+    pi[new_node] = PartitionInfo(count=count_out)
+
+    if count_out > 1:
+        # Add OrderedShuffle if we have multiple partitions
+        shuffle_options: dict[str, Any] = {}  # Unused for now
+        ordered_shuffle = OrderedShuffle(
+            new_node.schema,
+            new_node.by[0],
+            new_node.order[0],
+            new_node.null_order[0],
+            shuffle_options,
+            new_node,
+        )
+        pi[ordered_shuffle] = PartitionInfo(count=count_out)
+
+        # Add final Sort node
+        new_node = ir.reconstruct([ordered_shuffle])
+        pi[new_node] = PartitionInfo(count=count_out)
+
     return new_node, pi
 
 
-@generate_ir_tasks.register(Sort)
+def _local_quantile(df: DataFrame, by: NamedExpr, count: int) -> DataFrame:
+    return DataFrame(
+        [
+            Column(
+                plc.quantiles.quantile(
+                    by.evaluate(df),
+                    np.linspace(0.0, 1.0, count),
+                    # Use "nearest" for general dtype support
+                    interp=plc.types.interpolation.NEAREST,
+                    exact=False,
+                )
+            ).rename("quantiles")
+        ]
+    )
+
+
+def _global_quantile(dfs: list[DataFrame], count: int) -> Column:
+    return Column(
+        plc.quantiles.quantile(
+            _concat(dfs).column_map["quantiles"],
+            np.linspace(0.0, 1.0, count),
+            # Use "nearest" for general dtype support
+            interp=plc.types.interpolation.NEAREST,
+            exact=False,
+        )
+    )
+
+
+def _quantile_graph(
+    name_in: str,
+    by: NamedExpr,
+    count_in: int,
+    count_out: int,
+) -> tuple[str, MutableMapping[Any, Any]]:
+    """Make simple quantile graph."""
+    local_name = f"quantiles-local-{name_in}"
+    global_name = f"quantiles-{name_in}"
+
+    graph: MutableMapping[Any, Any] = {}
+    for pid in range(count_in):
+        graph[(local_name, pid)] = (
+            _local_quantile,
+            (name_in, pid),
+            by,
+            count_out,
+        )
+    graph[global_name] = (_global_quantile, list(graph.keys()), count_out)
+
+    return global_name, graph
+
+
+@generate_ir_tasks.register(OrderedShuffle)
 def _(
-    ir: Sort, partition_info: MutableMapping[IR, PartitionInfo]
+    ir: OrderedShuffle, partition_info: MutableMapping[IR, PartitionInfo]
 ) -> MutableMapping[Any, Any]:
-    # Step 1. find splits on each partition
-
-    # Step 2. Combine the splits in a single task
-
-    # Step 3. Shuffle (using the splits from step 2)
-
-    # Step 4. Local sort
+    # Step 1. Estimate global quantiles
+    (child,) = ir.children
+    name_in = get_key_name(child)
+    count_in = partition_info[child].count
+    count_out = partition_info[ir].count
+    if not ir.by.value.is_pointwise:
+        raise NotImplementedError()
+    name_quantile, graph = _quantile_graph(
+        name_in,
+        ir.by,
+        count_in,
+        count_out,
+    )
 
     raise NotImplementedError()
+
+    # # Step 2. Perform ordered shuffle
+    # name_out = get_key_name(ir)
+    # shuffle_graph = _ordered_shuffle_graph(
+    #     name_in,
+    #     name_out,
+    #     name_quantile,
+    #     ir.by,
+    #     ir.order,
+    #     count_in,
+    #     count_out,
+    # )
+
+    # graph.update(shuffle_graph)
+    # return graph
