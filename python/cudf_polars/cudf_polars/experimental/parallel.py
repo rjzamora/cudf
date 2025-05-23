474e80c94d80c929c94c3eed7690d3b396a91ac9
rjzamora <rzamora217@gmail.com>
2025-05-23 09:42:08 -0700

    save combined work

--- python/cudf_polars/cudf_polars/experimental/parallel.py
+++ python/cudf_polars/cudf_polars/experimental/parallel.py
@@ -38,6 +38,7 @@ from cudf_polars.experimental.dispatch import (
     generate_ir_tasks,
     lower_ir_node,
 )
+from cudf_polars.experimental.fusion import fuse_ir_graph
 from cudf_polars.experimental.utils import _concat, _lower_ir_fallback
 
 if TYPE_CHECKING:
@@ -87,8 +88,15 @@ def lower_ir_graph(
     --------
     lower_ir_node
     """
+    assert config_options.executor.name == "streaming", (
+        "'in-memory' executor not supported in 'lower_ir_graph'"
+    )
+
     mapper = CachingVisitor(lower_ir_node, state={"config_options": config_options})
-    return mapper(ir)
+    lowered_ir, partition_info = mapper(ir)
+    if config_options.executor.task_fusion:
+        return fuse_ir_graph(lowered_ir, partition_info)
+    return lowered_ir, partition_info
 
 
 def task_graph(
