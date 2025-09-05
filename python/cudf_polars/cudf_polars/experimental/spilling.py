91a770f02b20a093777bd944e899bea491bc6858
rjzamora <rzamora217@gmail.com>
2025-09-05 13:08:27 -0700

    add commented-out device_memory_available code

--- python/cudf_polars/cudf_polars/experimental/spilling.py
+++ python/cudf_polars/cudf_polars/experimental/spilling.py
@@ -13,9 +13,11 @@ if TYPE_CHECKING:
 
 try:
     # For now, we can only handle SpillableWrapper objects.
+    from rapidsmpf.buffer.buffer import MemoryType
     from rapidsmpf.integrations.dask.spilling import SpillableWrapper as Spillable
 
 except ImportError:
+    MemoryType = None  # type: ignore[no-redef]
 
     class Spillable:  # type: ignore[no-redef]
         """Placeholder for Spillable."""
@@ -33,6 +35,33 @@ def unspill_and_evaluate(
     *args: DataFrame | Spillable,
 ) -> DataFrame:
     """Call an IR.do_evaluate function with unspilled child inputs."""
+    # # Make headroom before executing the task (if possible)
+    # io_task = not args
+    # n_spilled = len(
+    #     [isinstance(obj, Spillable) and obj.mem_type() == MemoryType.HOST for obj in args]
+    # ) if MemoryType is not None else 0
+    # if io_task or n_spilled:
+    #     try:
+    #         from rapidsmpf.integrations.dask.core import get_worker_context
+
+    #         ctx = get_worker_context()
+    #         with ctx.lock:
+    #             headroom = 1_000_000_000 * (n_spilled + 1)
+    #             available = ctx.br.device_memory_available
+    #             if available < headroom:
+    #                 spilled = ctx.br.spill_manager.spill_to_make_headroom(headroom=headroom)
+    #                 if io_task and (available + spilled) < headroom:
+    #                     # Don't execute an IO task yet if we can't make headroom.
+    #                     # We can't do this for non-IO tasks, because we don't want
+    #                     # to move the input data.
+    #                     from distributed.exceptions import Reschedule
+
+    #                     raise Reschedule()
+
+    #     except (ImportError, ValueError):
+    #         pass
+
+    # Execute the task
     output = do_evaluate(*non_child_args, *(unspill(arg) for arg in args))
     if spillable_output:
         return Spillable(on_device=output)
