53c9dd3f61e76edfb2fb4d4ef417975f04133a14
rjzamora <rzamora217@gmail.com>
2025-09-08 05:46:53 -0700

    uncomment experimental headroom logic

--- python/cudf_polars/cudf_polars/experimental/spilling.py
+++ python/cudf_polars/cudf_polars/experimental/spilling.py
@@ -35,31 +35,40 @@ def unspill_and_evaluate(
     *args: DataFrame | Spillable,
 ) -> DataFrame:
     """Call an IR.do_evaluate function with unspilled child inputs."""
-    # # Make headroom before executing the task (if possible)
-    # io_task = not args
-    # n_spilled = len(
-    #     [isinstance(obj, Spillable) and obj.mem_type() == MemoryType.HOST for obj in args]
-    # ) if MemoryType is not None else 0
-    # if io_task or n_spilled:
-    #     try:
-    #         from rapidsmpf.integrations.dask.core import get_worker_context
+    # Make headroom before executing the task (if possible)
+    io_task = not args
+    n_spilled = (
+        len(
+            [
+                isinstance(obj, Spillable) and obj.mem_type() == MemoryType.HOST
+                for obj in args
+            ]
+        )
+        if MemoryType is not None
+        else 0
+    )
+    if io_task or n_spilled:
+        try:
+            from rapidsmpf.integrations.dask.core import get_worker_context
 
-    #         ctx = get_worker_context()
-    #         with ctx.lock:
-    #             headroom = 1_000_000_000 * (n_spilled + 1)
-    #             available = ctx.br.device_memory_available
-    #             if available < headroom:
-    #                 spilled = ctx.br.spill_manager.spill_to_make_headroom(headroom=headroom)
-    #                 if io_task and (available + spilled) < headroom:
-    #                     # Don't execute an IO task yet if we can't make headroom.
-    #                     # We can't do this for non-IO tasks, because we don't want
-    #                     # to move the input data.
-    #                     from distributed.exceptions import Reschedule
+            ctx = get_worker_context()
+            with ctx.lock:
+                headroom = 1_000_000_000 * (n_spilled + 1)
+                available = ctx.br.memory_available(MemoryType.DEVICE)
+                if available < headroom:
+                    spilled = ctx.br.spill_manager.spill_to_make_headroom(
+                        headroom=headroom
+                    )
+                    if io_task and (available + spilled) < headroom:
+                        # Don't execute an IO task yet if we can't make headroom.
+                        # We can't do this for non-IO tasks, because we don't want
+                        # to move the input data.
+                        from distributed.exceptions import Reschedule
 
-    #                     raise Reschedule()
+                        raise Reschedule()
 
-    #     except (ImportError, ValueError):
-    #         pass
+        except (ImportError, ValueError):
+            pass
 
     # Execute the task
     output = do_evaluate(*non_child_args, *(unspill(arg) for arg in args))
