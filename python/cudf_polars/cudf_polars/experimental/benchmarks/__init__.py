2fb74b71a65582298c57bc1b0d38e31ca6547125
rjzamora <rzamora217@gmail.com>
2026-02-05 11:32:12 -0800

    adding helper utlity to visualize traced plans

--- python/cudf_polars/cudf_polars/experimental/benchmarks/__init__.py
+++ python/cudf_polars/cudf_polars/experimental/benchmarks/__init__.py
@@ -1,4 +1,12 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES.
 # SPDX-License-Identifier: Apache-2.0
 
 """Experimental benchmarks."""
+
+from __future__ import annotations
+
+from cudf_polars.experimental.benchmarks import explain_trace
+
+__all__ = [
+    "explain_trace",
+]
